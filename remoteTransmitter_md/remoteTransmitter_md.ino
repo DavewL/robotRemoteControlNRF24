6d28d80af3d4a5e46573a19f46cd795b7d76af59
root <dave.lombardo@gmail.com>
2015-11-14 17:56:54 -0500

    upload files

---
+++ remoteTransmitter_md/remoteTransmitter_md.ino
@@ -0,0 +1,26 @@
+#include <RF24_config.h>
+#include <printf.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <SPI.h>
+
+/****************** User Config ***************************/
+/***      Set this radio as radio number 0 or 1         ***/
+bool radioNumber = 0;
+
+/* Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 7 & 8 */
+RF24 radio(7,8);
+/**********************************************************/
+
+byte addresses[][6] = {"1Node","2Node"};
+
+void setup() {
+  radio.begin();
+
+  //RF24_PA_MAX is default.
+  radio.setPALevel(RF24_PA_LOW);
+  
+  // Open a writing and reading pipe on each radio, with opposite addresses
+  if(radioNumber){
+    radio.openWritingPipe(addresses[1]);
+    radio.openReadingPipe(1,addresses[0]);
